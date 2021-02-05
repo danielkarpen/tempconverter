417d25049b0bb1f6d93dbcfd38d5c4fdd3c4ba05
Daniel <danielkarpen97@gmail.com>
2021-02-05 15:57:30 -0600

    added TODOs and starter code

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// TODO: when i click and start typing in either input, update the other's input value with the converted temperature
+
+// hints 1. keyup event
+// 2 use function keyword so that you can use 'this'
