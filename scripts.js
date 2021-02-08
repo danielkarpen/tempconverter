5af20ff065058d5e31ca42dcdcba3f99af0dddd5
Daniel <danielkarpen97@gmail.com>
2021-02-08 17:18:30 -0600

    solution

--- scripts.js
+++ scripts.js
@@ -2,3 +2,16 @@
 
 // hints 1. keyup event
 // 2 use function keyword so that you can use 'this'
+import convert from "./lib.js";
+
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach((input) => {
+  input.addEventListener("input", function handleKeyUp() {
+    if (this.id === "degF") {
+      inputs[0].value = convert(this.value, "toCelsius");
+    } else {
+      inputs[1].value = convert(this.value, "toFahrenheit");
+    }
+  });
+});
